91d44ce56030dc9f7173526af625e7019dc38191
Alex Parker <7272359+ajp-io@users.noreply.github.com>
2025-09-23 11:19:43 -0700

    Remove blue dot and improve selected state styling

--- src/components/wizard/configuration/components/ConfigStepper.tsx
+++ src/components/wizard/configuration/components/ConfigStepper.tsx
@@ -113,10 +113,6 @@ const ConfigStepper: React.FC<ConfigStepperProps> = ({
                       </span>
                     )}
                   </div>
-                  
-                  {isCurrent && (
-                    <div className="w-2 h-2 rounded-full" style={{ backgroundColor: themeColor }} />
-                  )}
                 </div>
               </div>
             </button>
